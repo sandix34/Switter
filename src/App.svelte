5267e51829bc46e49ff2eb1898dee81910e10bff
sandrine <sandrinezocca@gmail.com>
2019-06-13 16:47:23 +0200

    Mettre à jour une variable et l'afficher

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+  export let name;
+  let message = "";
+
+  function updateMessage(event) {
+	console.log(event.target.value);
+	message = event.target.value;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>{name}</h1>
+<textarea cols="50" rows="5" on:input={updateMessage} />
+<div>
+	<h3>Preview</h3>
+	{message}
+</div>
