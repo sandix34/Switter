46c4dbaaab43cdd276bebc5cb5d3826c4a6cd1ca
sandrine <sandrinezocca@gmail.com>
2019-06-14 12:51:00 +0200

    Parcourir une collection de messages côté template

--- src/App.svelte
+++ src/App.svelte
@@ -34,4 +34,12 @@
 	<h3>Preview</h3>
 	{message}
 </div>
+<div>
+  <h2>Messages</h2>
+  { #each messages as message }
+    <div>By { message.author }</div>
+    <div>{ message.text }</div>
+    <hr>
+  { /each }
+</div>
 
