9a44b264fc8b0fa1a7adb6d233ba381f63b16379
sandrine <sandrinezocca@gmail.com>
2019-06-14 12:41:10 +0200

    Gérer le 'click' event pour ajouter un nouveau message

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script>
   export let name;
   let message = "";
+  let messages = [];
 
   function updateMessage(event) {
 	console.log(event.target.value);
 	message = event.target.value;
+  }
+
+  function saveMessage() {
+    const newMessage= {
+      id: Date.now(),
+      text: message,
+      author: 'Sandrine'
+    };
+    messages = [newMessage, ...messages]
+    console.log('messages', messages );
+    
   }
 </script>
 
@@ -16,7 +28,10 @@
 
 <h1>{name}</h1>
 <textarea cols="50" rows="5" on:input={updateMessage} />
+<br>
+<button on:click={ saveMessage }>send</button>
 <div>
 	<h3>Preview</h3>
 	{message}
 </div>
+
